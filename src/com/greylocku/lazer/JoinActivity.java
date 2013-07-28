9fc222a9132fdfde76bb2794c2f9e3ab813f66f8
Lance Co Ting Keh <lance@box.com>
2013-07-27 17:55:02 -0700

    Working parse

--- src/com/greylocku/lazer/JoinActivity.java
+++ src/com/greylocku/lazer/JoinActivity.java
@@ -1,5 +1,10 @@
 package com.greylocku.lazer;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +15,22 @@ public class JoinActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_join);
+		
+		//Initialize parse
+		// Add your initialization code here
+		Parse.initialize(this, "ZAxTBMrNWKxLc1vQT6s1RJWRK2ytQoY4TGCNmdXV", "nmYDNx1IghjEjPyXgTJyfSUiOxIyeEEGjqbMX2U7"); 
+
+		ParseACL defaultACL = new ParseACL();	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		ParseACL.setDefaultACL(defaultACL, true);
+		
+		//demo saving
+		ParseObject gameScore = new ParseObject("GameScore");
+		gameScore.put("score", 1337);
+		gameScore.put("playerName", "Sean Plott");
+		gameScore.put("cheatMode", false);
+		gameScore.saveInBackground();
 	}
 
 	@Override
