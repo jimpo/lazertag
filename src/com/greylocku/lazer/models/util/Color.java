5c52fe47a051e4c288c5053831387ddbe78f9ca5
Lance Co Ting Keh <lance@box.com>
2013-07-28 02:49:57 -0700

    Borken sound shiz

---
+++ src/com/greylocku/lazer/models/util/Color.java
@@ -0,0 +1,14 @@
+package com.greylocku.lazer.models.util;
+
+public class Color {
+
+	public Double hue;
+	public Double saturation;
+	public Double value;
+	
+	public Color(Double h, Double s, Double v){
+		this.hue = h;
+		this.saturation = s;
+		this.value = v;
+	}
+}
